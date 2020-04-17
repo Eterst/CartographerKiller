4d99075e37c5338bc8eb6bcc71347c63dfb17017
Jonder <Jonder09@hotmail.com>
2020-04-16 22:31:21 -0600

    Cambiado codigo obsoleto por un nuevo tipo de solucion

--- src/me/Eters/CartographerKiller/CartographerListener.java
+++ src/me/Eters/CartographerKiller/CartographerListener.java
@@ -10,21 +10,33 @@ import org.bukkit.event.entity.VillagerAcquireTradeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+import org.bukkit.entity.Villager.Profession;
 
 public class CartographerListener implements Listener{
-	
+	/* Event Handler Absoleto
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event)
 	{
 		event.setJoinMessage("Welcome, " + event.getPlayer().getName() + "!");
 	}
-	
+	*/
 	@EventHandler
 	public void onVillagerAcquireTradeEvent(VillagerAcquireTradeEvent event)
 	{
 		Bukkit.getPlayer("Eters").sendMessage(event.getRecipe().getResult().getData().toString());
 		
+		if (event.getEntityType().equals(EntityType.VILLAGER))
+		{
+			Villager villager = (Villager) event.getEntity();
+			if (villager.getProfession().compareTo(Profession.CARTOGRAPHER) == 0) {
+				event.getRecipe().setVillagerExperience(0);
+			}
+		}
+		
+		/* Codigo Viejo Obsoleto
 		if (event.getRecipe().getResult().getData().getItemType().equals(Material.LEGACY_MAP)) {//.toString().equalsIgnoreCase("LEGACY_MAP")) {
 			Bukkit.getPlayer("Eters").sendMessage("Se encontro mapa!!");
 			ItemStack map = new ItemStack(Material.LEGACY_EMPTY_MAP);
@@ -34,7 +46,6 @@ public class CartographerListener implements Listener{
 			//List<ItemStack> ingredients;
 			event.setRecipe(recipe);
 		}
+		*/
 	}
-	
-	//VillagerAcquireTradeEvent
 }
